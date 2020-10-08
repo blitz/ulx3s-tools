8c2a3213111e1322fc2e6d4d3f5876c145b80914
Julian Stecklina <js@alien8.de>
2020-10-08 23:40:39 +0200

    Add yosys from git

--- default.nix
+++ default.nix
@@ -2,7 +2,11 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
 in {
-  fujprog = pkgs.callPackage ./tools/fujprog.nix {
-    fujprogSource = sources.fujprog;
-  };
+  fujprog =
+    pkgs.callPackage ./tools/fujprog.nix { fujprogSource = sources.fujprog; };
+
+  yosys = pkgs.yosys.overrideAttrs (old: {
+    version = "20201008-git";
+    src = sources.yosys;
+  });
 }
